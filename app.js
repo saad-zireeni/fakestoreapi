bfb4a7818ae42ed93322c24c287604f6e0ec5851
saad-zireeni <saad.alzireeni@gmail.com>
2023-04-17 01:33:29 +0300

    update

--- app.js
+++ app.js
@@ -14,44 +14,33 @@ fetch('https://fakestoreapi.com/products?limit=20')
     products = data.map(item => new Product(item.title, item.price, item.description, item.image));
     products.forEach(item => {
 
-
       const cardsHolder = document.getElementById("cards-holder");
-      
-      
+
       const card = document.createElement("div");
       card.classList.add("card");
       cardsHolder.appendChild(card);
-
-      const image = document.createElement('img');
-      image.classList.add("card-img-top");
-      image.classList.add("img-fluid");
-      image.style.maxWidth = "100%";
-      image.style.maxHeight = "200px";
+      
+      const image = document.createElement('img'); 
       image.src = item.image;
       card.appendChild(image);
-
-
+      
       const cardBody = document.createElement("div");
       cardBody.classList.add("card-body");
       card.appendChild(cardBody);
-
-
-    
-      const title = document.createElement('h5');
+      
+      const title = document.createElement('h2');
       title.textContent = item.title;
       cardBody.appendChild(title);
-
-
+      
       const description = document.createElement('p');
       description.textContent = item.description;
       cardBody.appendChild(description);
-
-
-      const price = document.createElement('p');
+      
+      const price = document.createElement('span');
       price.classList.add("price");
       price.textContent = `Price : ${item.price}`;
       cardBody.appendChild(price);
-
+      
       
     });
   });
